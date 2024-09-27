b0e65c2b2551ba82e82363d218f676a1970fa2ac
naseefmarutha <naseefmarutha02@gmail.com>
2024-09-27 18:04:51 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+function sentMail(){
+    var params={
+        name:document.getElementById("name").value ,
+        email:document.getElementById("email").value,
+        subject:document.getElementById("subject").value,
+    }
+}
